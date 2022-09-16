9204d3c76ace674da909e5709378203717085c7d
Lucas <aranchipe98@gmail.com>
2022-09-16 19:16:04 -0300

    correcao da palavra cadastro

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,30 +4,30 @@ import './style.css'
 
 
 function Login() {
-   const [username, setUsername] = React.useState('');
-   const [password, setPassword] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
 
-   function handleSubmit(event) {
-    event.preventDefault();
-   }
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
 
     return (
         <section>
             <Link to="/Cadastro">Cadastro</Link>
-            
+
             <h1>Login</h1>
             <form action="" onSubmit={handleSubmit}>
-                <input 
-                 value={username}
-                 type="text" onChange={({target}) => setUsername
-                (target.value)}/>
-                <input 
-                 value={password}
-                 type="text" onChange={({target}) => setPassword
-                (target.value)}/>
+                <input
+                    value={username}
+                    type="text" onChange={({ target }) => setUsername
+                        (target.value)} />
+                <input
+                    value={password}
+                    type="text" onChange={({ target }) => setPassword
+                        (target.value)} />
                 <button>Entrar</button>
             </form>
-           
+
         </section>
     )
 }
