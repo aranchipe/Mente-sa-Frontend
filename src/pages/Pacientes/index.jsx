c1b42d31f928a08da3cac1fb6ce3ac29a57d07ac
Denise Costa <denisinhacosta@hotmail.com>
2022-09-19 11:37:48 -0300

    Tabela Pacientes

--- src/pages/Pacientes/index.jsx
+++ src/pages/Pacientes/index.jsx
@@ -1,11 +1,38 @@
-import './style.css';
 import MenuLateral from '../../components/MenuLateral';
+import TabelaPacientes from '../../components/TabelaPacientes';
+import axios from '../../services/axios';
+import { getItem } from '../../utils/storage'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import lupa from '../../assets/lupa.svg'
+import plus from '../../assets/plus.svg'
 
 function Pacientes() {
+    const token = getItem('token')
+    const [pacientes, setPacientes] = useState([])
+
+    useEffect(() => {
+        listarPacientes()
+    })
+    async function listarPacientes() {
+        try {
+            const response = await axios.get('/paciente', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+
+                }
+            })
+            setPacientes(response.data)
+
+        } catch (error) {
+
+        }
+    }
     return (
         <div className="Main">
             <h1>Pacientes</h1>
-            <MenuLateral></MenuLateral>
+            <MenuLateral />
+            <TabelaPacientes pacientes={pacientes} />
         </div>
     );
 }
