0be758bfdb5a9b9335999c6ea00d73074087363b
igor <igor.valerio@hotmail.com>
2022-09-19 02:29:06 -0300

    Estilizacao login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './style.css'
-import axios from '../../services/axios'
-import { setItem, getItem } from '../../utils/storage'
-import { notifyError } from '../../utils/toast'
-import { useNavigate } from 'react-router-dom'
+import './style.css';
+import axios from '../../services/axios';
+import { setItem, getItem } from '../../utils/storage';
+import { notifyError } from '../../utils/toast';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import mindfull from "../../assets/mindfull.png";
+import { Button, FormGroup } from 'reactstrap';
 
 function Login() {
     const navigate = useNavigate()
@@ -50,27 +53,62 @@ function Login() {
     }
 
     return (
-        <section>
-            <Link to="/Cadastro">Cadastro</Link>
 
-            <h1>Login</h1>
-            <form action="" onSubmit={handleSubmit}>
-                <input
-                    name='email'
-                    value={form.email}
-                    type="text"
-                    onChange={handleChangeInput}
-                />
-                <input
-                    name='senha'
-                    value={form.senha}
-                    type="password"
-                    onChange={handleChangeInput}
-                />
-                <button>Entrar</button>
-            </form>
-
-        </section>
+        <div class="container col-11 col-md-9" id="form-container">
+            <div class="row align-items-center gx-5">
+                <div class="col-md-6 order-md-2">
+                    <section>
+                        <h1>Mente Sa</h1>
+                        <h5>Bem vindo ao sistema</h5>
+                        <p>Por favor entre com sua conta</p>
+                        <form action="" onSubmit={handleSubmit}>
+                            <div class="form-floating mb-3">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    class="form-control"
+                                    value={form.email}
+                                    type="email"
+                                    onChange={handleChangeInput}
+                                    placeholder="Digite o seu e-mail"
+                                />
+                                <label for="email" class="form-label">Digite o seu e-mail</label>
+                            </div> 
+                            <div class="form-floating mb-3">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    class="form-control"
+                                    value={form.password}
+                                    type="password"
+                                    onChange={handleChangeInput}
+                                    placeholder="Digite a sua senha"
+                                />
+                                <label for="password" class="form-label">Digite a sua senha</label>
+                            </div> 
+                            <div class="form-check">
+                                <input class="form-check-input" checked type="checkbox" id="lembrar" value="option1" />
+                                <label class="form-check-label" for="lembrar">Lembrar usuario</label>
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="Entrar"/>   
+                        </form>
+                    </section>
+                </div>
+                <div class="col-md-6 order-md-1">
+                    <div class="col-12">
+                        <img src={mindfull} alt="" srcset="" class="img-fluid"/>
+                    </div>
+                    <div class="col-12 links" id="link-container">
+                        <div class="d-flex justify-content-between">
+                            <a href="">Esqueci minha senha</a>
+                            <Link to="/Cadastro">Criar Conta</Link>
+                        </div>
+                                  
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     )
 }
 
