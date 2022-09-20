9daf08bc359771ab8e2015ae6c21251024e07016
Lucas <aranchipe98@gmail.com>
2022-09-19 21:45:02 -0300

    funcionalidade de editar paciente

--- src/components/TabelaPacientes/index.jsx
+++ src/components/TabelaPacientes/index.jsx
@@ -2,31 +2,62 @@ import './style.css';
 import acao from '../../assets/acao.svg'
 import editIcon from '../../assets/edit-icon.svg'
 import deleteIcon from '../../assets/delete-icon.svg'
+import ModalPacientes from "../../components/ModalPacientes";
+import { useState } from 'react';
 
-function TabelaPacientes({ pacientes }) {
-  return (
-      <div className='container-paciente'>
-          <table className='table-paciente'>
-              <thead >
-                  <tr>
-                      <th>Nome</th>
-                      <th>Endereço</th>
-                      <th>E-mail</th>
-                      <th>Gênero</th>                      
-                      <th>Ações</th>
-                  </tr>
-              </thead>
-              <tbody>
-                {pacientes.map((item) => (
+function TabelaPacientes({
+    pacientes,
+    action,
+    setModalCadastrar,
+    setModalEditar,
+    setModalExcluir,
+    modalCadastrar,
+    modalEditar,
+    modalExcluir,
+    setModalAction
+}) {
+    const [pacienteAtual, setPacienteAtual] = useState()
+    return (
+        <div className='container-paciente'>
+            {modalCadastrar === true || modalEditar === true || modalExcluir === true ? (
+                <ModalPacientes
+                    action={action}
+                    setModalCadastrar={setModalCadastrar}
+                    setModalEditar={setModalEditar}
+                    setModalExcluir={setModalExcluir}
+                    pacienteAtual={pacienteAtual}
+                />
+            ) : (
+                ""
+            )}
+            <table className='table-paciente'>
+                <thead >
+                    <tr>
+                        <th>Nome</th>
+                        <th>Endereço</th>
+                        <th>E-mail</th>
+                        <th>Gênero</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {pacientes.map((item) => (
                         <tr key={item.id}>
                             <td>{item.nome}</td>
                             <td>{item.endereco}</td>
                             <td>{item.email}</td>
-                            <td>{item.genero}</td>                                              
+                            <td>{item.genero}</td>
                             <td>
                                 <div className='action-icons'>
                                     <img src={acao} alt='acao' />
-                                    <img src={editIcon} alt='editIcon' />
+                                    <img
+                                        src={editIcon}
+                                        alt='editIcon'
+                                        onClick={() => {
+                                            setModalAction('editar');
+                                            setModalEditar(true);
+                                            setPacienteAtual(item);
+                                        }} />
                                     <img src={deleteIcon} alt='deleteIcon' />
 
                                 </div>
@@ -35,15 +66,16 @@ function TabelaPacientes({ pacientes }) {
 
                     ))}
 
-                 
-                  
-              </tbody>
-
-          </table>
 
 
-      </div>
-  )
+                </tbody>
+
+            </table>
+
+
+        </div>
+
+    )
 }
 
 export default TabelaPacientes
--- src/pages/Pacientes/index.jsx
+++ src/pages/Pacientes/index.jsx
@@ -7,10 +7,29 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import lupa from '../../assets/lupa.svg'
 import plus from '../../assets/plus.svg'
+import ModalPacientes from "../../components/ModalPacientes";
+import {
+    Main,
+    Container,
+    Header,
+    Table,
+    IconEye,
+    IconPencil,
+    IconTrash,
+    Tooltip,
+    TooltipText,
+} from "../Pacientes2/style";
+import { BsFillPersonPlusFill, BsSearch } from "react-icons/bs";
+
 
 function Pacientes() {
     const token = getItem('token')
     const [pacientes, setPacientes] = useState([])
+    const [modalCadastrar, setModalCadastrar] = useState(false);
+    const [modalEditar, setModalEditar] = useState(false);
+    const [modalExcluir, setModalExcluir] = useState(false);
+    const [modalAction, setModalAction] = useState("");
+
 
     useEffect(() => {
         listarPacientes()
@@ -32,8 +51,36 @@ function Pacientes() {
     return (
         <div className="Main">
             <h1>Pacientes</h1>
-            <MenuLateral />
-            <TabelaPacientes pacientes={pacientes} />
+            < MenuLateral />
+            <Header>
+                <h1>Meus Pacientes</h1>
+                <div>
+                    <span>
+                        <BsSearch />
+                    </span>
+                    <input type="text" name="" id="" placeholder="Pesquisar" />
+                </div>
+                <button
+                    onClick={() => {
+                        setModalCadastrar(true);
+                        setModalAction("cadastrar");
+                    }}
+                >
+                    <BsFillPersonPlusFill size={20} />
+                    Novo Paciente
+                </button>
+            </Header>
+            <TabelaPacientes
+                action={modalAction}
+                setModalCadastrar={setModalCadastrar}
+                setModalEditar={setModalEditar}
+                setModalExcluir={setModalExcluir}
+                pacientes={pacientes}
+                modalCadastrar={modalCadastrar}
+                modalEditar={modalEditar}
+                modalExcluir={modalExcluir}
+                setModalAction={setModalAction}
+            />
         </div>
     );
 
