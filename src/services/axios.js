b5be9869d3b1c0be1e2223692911d74e5ecb1e7a
Lucas <aranchipe98@gmail.com>
2022-09-22 22:11:02 -0300

    alguns ajustes

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 export default axios.create({
-    /* baseURL: 'http://localhost:3334', */
-    baseURL: 'https://mente-sa.herokuapp.com',
+    baseURL: 'http://localhost:3334',
+    /*  baseURL: 'https://mente-sa.herokuapp.com', */
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
